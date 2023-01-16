ab2e1e8f647371fb8bd047b54b2a7634adc1d847
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2023-01-16 06:45:18 +0300

    Add window with opening deposit

--- bank_app/windows/main_menu.py
+++ bank_app/windows/main_menu.py
@@ -5,6 +5,7 @@ from bank_app.user import User
 from bank_app.windows.add_money import AddMoneyWindow
 from bank_app.windows.balance_window import BalanceWindow
 from bank_app.windows.deposits_window import DepositsWindow
+from bank_app.windows.open_deposit import OpenDepositWindow
 
 
 class MainMenu(QMainWindow):
@@ -18,11 +19,13 @@ class MainMenu(QMainWindow):
         self.balance_window = None
         self.deposits_window = None
         self.add_money_window = None
+        self.open_deposit_window = None
 
         self.exitPushButton.clicked.connect(self.exit_to_auth)
         self.addPushButton.clicked.connect(self.add_money)
         self.statusPushButton.clicked.connect(self.show_balance)
         self.listDepositsPushButton.clicked.connect(self.show_deposits)
+        self.addDepositPushButton.clicked.connect(self.add_deposit)
 
     def show_balance(self):
         self.hide()
@@ -38,6 +41,11 @@ class MainMenu(QMainWindow):
         self.hide()
         self.add_money_window = AddMoneyWindow(self, self.user)
         self.add_money_window.show()
+
+    def add_deposit(self):
+        self.hide()
+        self.open_deposit_window = OpenDepositWindow(self, self.user)
+        self.open_deposit_window.show()
 
     def exit_to_auth(self):
         self.auth_window.show()
---
+++ bank_app/windows/open_deposit.py
@@ -0,0 +1,28 @@
+from PyQt5 import uic
+
+from bank_app.deposit import Deposit
+from bank_app.user import User
+from bank_app.utils.window import create_dialog
+from bank_app.utils.users import update_user
+from bank_app.windows.main_menu_element import MainMenuElement
+
+
+class OpenDepositWindow(MainMenuElement):
+    def __init__(self, previous_window, user: User):
+        super().__init__(previous_window)
+        uic.loadUi("bank_app/windows/ui/open_deposit.ui", self)
+
+        self.user = user
+        self.previous_window = previous_window
+
+        self.enterPushButton.clicked.connect(self.open_deposit)
+        self.exitPushButton.clicked.connect(self.exit_to_menu)
+
+    def open_deposit(self):
+        date_obj = self.dateEdit.date()
+        day, month, year = date_obj.day(), date_obj.month(), date_obj.year()
+        date = f"{day}.{month}.{year}"
+        type = self.typeComboBox.currentText()
+        self.user.deposits.append(Deposit(type, date))
+        update_user(self.user)
+        create_dialog(self, "Открытие вклада", "Вклад успешно открыт!")
